475436e3459d2dc714a454dd08b3ea558b936978
Stuart Reckase <sdreckase@gmail.com>
2023-05-19 17:16:13 -0700

    added baseTileLayer

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+import VectorTileLayer from "@arcgis/core/layers/VectorTileLayer";
 
 import "./style.css";
 
+const tileBaseLayer = new VectorTileLayer({
+  url: 'https://tiles.arcgis.com/tiles/qboYD3ru0louQq4F/arcgis/rest/services/WWUbasemap/VectorTileServer'
+})
+
 const map = new Map({
   basemap: "streets-vector",
+  layers: tileBaseLayer
+
 });
 
 const view = new MapView({
