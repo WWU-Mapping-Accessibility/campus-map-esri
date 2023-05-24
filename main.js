b61902e0a2e1d4419096ca3cb3b0c951cd0839cd
Stuart Reckase <sdreckase@gmail.com>
2023-05-24 06:30:27 -0700

    hashes

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ import VectorTileLayer from "@arcgis/core/layers/VectorTileLayer";
 
 import "./style.css";
 
+//// DEFAULTS
+
+let zoom = 15;
+let center = [-122.48614277687422, 48.732800397930795];
+
+
+/// Get location from hash
+
+if (window.location.hash !== ''){
+    // try to restore center, zoom-level and rotation from the URL
+    const hash = window.location.hash.replace('#map=', '');
+    const parts = hash.split('/');
+    if (parts.length === 3) {
+      zoom = parseFloat(parts[0]);
+      center = [parseFloat(parts[1]), parseFloat(parts[2])];
+    }
+}
+  
+
+
+
+
 const tileBaseLayer = new VectorTileLayer({
   url: 'https://tiles.arcgis.com/tiles/qboYD3ru0louQq4F/arcgis/rest/services/WWUbasemap/VectorTileServer'
 })
@@ -17,13 +39,34 @@ const map = new Map({
 const view = new MapView({
   container: "map",
   map: map,
-  zoom: 15,
-  center: [-122.48614277687422, 48.732800397930795],
+  zoom: zoom,
+  center: center,
   constraints: {
     snapToZoom: false,
+
   },
   
 });
+
+
+/// Set location hash
+
+const updateURLHash = function(){
+  const center = view.get(center);
+  const hash = 
+  '#map=' +
+  view.get(zoom) +
+  '/' +
+  center[0]
+  + 
+  '/' +
+  center[1]
+
+  return hash;
+}
+
+
+
 
 const pointerCoord = document.getElementById('info');
 
@@ -34,5 +77,5 @@ view.on('pointer-move', (evt) => {
 });
 
 view.on('click', (evt) => {
-  console.log(pointerCoord.innerHTML)
+  console.log()
 });
