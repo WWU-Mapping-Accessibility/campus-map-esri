44b2bc54549449fac18391f85a146751d7c15eea
Stu Reckase <sdreckase@gmail.com>
2023-06-05 10:59:14 -0700

    hash locations and Layers

--- main.js
+++ main.js
@@ -12,62 +12,8 @@ import "./style.css";
 /* DEFAULTS & CONFIGS */
 let zoom = 15;
 let center = [-122.48614277687422, 48.732800397930795];
-
-// Format: "ABV": [Lon, Lat]
-const customPlaces = {
-
-}
-
-/* Set location from hash function*/
-const getLocationFromHash = function(places) {
-  // try to restore center, zoom-level and rotation from the URL
-  const hash = window.location.hash.replace('#wwu=', '');
-  const parts = hash.split('&');
-  // Parse URL Hash
-  if (parts.length === 1) {
-    const ident = parts[0]
-    if (parts[0] in Object.assign({},places,customPlaces)){
-      return({
-        zoom: 18.5,
-        center: [places[ident][0], places[ident][1]]
-      });
-    };
-  };
-};
-
-
-/* Queries the building info layer and places every location into the places dictionary*/
-const getPlaces = function(buildings){
-  // Creates a new Promise that will resolve with dictionary
-  return new Promise((resolve, reject) => {
-    buildings.queryFeatures().then(result => {
-      // Reduces query to just the abv and coordinates
-      const featuresProc = (result.features).reduce((acc, feature) => {
-        acc[feature.attributes.abv] = [feature.geometry.longitude,feature.geometry.latitude];
-        return acc;
-          }, {});
-            resolve(featuresProc);
-      }).catch(error => {reject(error);})
-    }
-  );
-};
-
-// Calls the getPlaces, getLocation functions to set the center based on the map
-const setLocationFromHash = function(view){
-  if (window.location.hash !== ''){
-    try{
-      // This will execute async to the map loading, so it may take a sec to snap to the new location.
-      // Query speed dependent
-      getPlaces(buildingInfo5k).then(result => 
-        getLocationFromHash(result)).then(result => 
-          view.goTo(result));
-    } catch(error) {
-      console.error(error);
-    };
-  };
-};
-
-
+const windowHash = window.location.hash.replace('#wwu=', '').split('&');
+console.log(windowHash);
 
 /* Create Layers */
 const tileBaseLayer = new VectorTileLayer({
@@ -82,18 +28,90 @@ const buildingInfo5k = new FeatureLayer({
   url: 'https://services.arcgis.com/qboYD3ru0louQq4F/arcgis/rest/services/Building_Info__5k/FeatureServer/2',
 });
 
+const searchPoints = new FeatureLayer({
+  url: 'https://services.arcgis.com/qboYD3ru0louQq4F/arcgis/rest/services/search_pts/FeatureServer',
+});
+
+
 const dummyBasemap = new FeatureLayer({
   url: 'https://services.arcgis.com/qboYD3ru0louQq4F/arcgis/rest/services/DummyBasemapForLegend/FeatureServer',
   title: '',
 });
 
-/* Layers to Add */
-const layers = [dummyBasemap, tileBaseLayer, buildingAccInfo100k, buildingInfo5k];
+const defaultLayers = [dummyBasemap, tileBaseLayer, buildingAccInfo100k, buildingInfo5k];
+
+// Format: "ABV": [Lon, Lat]
+const customPlaces = {
+
+}
+
+/* Processes the hash and runs the correct functions based on hash length */
+
+const hashActions = function(hash=windowHash) {
+
+  if (hash.length >= 1){
+    /* Only triggers the setLocation function once the view is ready -- otherwise view may reset the center to defaults */
+    view.watch('ready', () => setLocationFromHash(view));
+  };
+
+  // Sets active layers to the ones in the URL
+  if (hash.length >= 2){
+    const enabledLayersString = hash[1].split('/');
+    // eval will use strings as variables
+    map.addMany(enabledLayersString.map(i => eval(i)));
+  } else {
+    map.addMany(defaultLayers);
+  };
+
+};
+
+/* Gets the location definined in the hash*/
+const getLocationFromHash = function(places, hash=windowHash) {
+
+  const ident = hash[0]
+  if (ident in Object.assign({}, places, customPlaces)){
+    return({
+      zoom: 18.5,
+      center: [places[ident][0], places[ident][1]]
+    });
+  };
+};
+
+/* Queries the search info layer and places every location into the places dictionary*/
+const getPlaces = function(layer){
+  // Creates a new Promise that will resolve with dictionary
+  return new Promise((resolve, reject) => {
+    layer.queryFeatures().then(result => {
+      // Reduces query to just the abv and coordinates
+      const featuresProc = (result.features).reduce((acc, feature) => {
+        acc[feature.attributes.Abv] = [feature.geometry.longitude,feature.geometry.latitude];
+        return acc;
+          }, {});
+            resolve(featuresProc);
+      }).catch(error => {reject(error);})
+    }
+  );
+};
+
+// Uses the getPlaces, getLocation functions to set the center based on the map
+const setLocationFromHash = function(view){
+  if (window.location.hash !== ''){
+    try{
+      // This will execute async to the map loading, so it may take a sec to snap to the new location.
+      // Query speed dependent
+      getPlaces(searchPoints).then(result => 
+        getLocationFromHash(result)).then(result => 
+          view.goTo(result));
+    } catch(error) {
+      console.error(error);
+    };
+  };
+};
+
 
 /* Creates the Map and View */
 const map = new Map({
   basemap: "streets-vector",
-  layers: layers
 
 });
 
@@ -108,7 +126,7 @@ const view = new MapView({
   },
 });
 
-
+hashActions();
 /* Widgets!! */
 
 /* Locate widget using a simple marker as the symbol (Prob change) */
@@ -135,8 +153,8 @@ const expand = new Expand({
 });
 
 
-/* Only triggers the setLocation function once the view is ready -- otherwise view may reset the center to defaults */
-view.watch('ready', () => setLocationFromHash(view));
+
+
 
 view.ui.add(locate, 'top-left');
 view.ui.add(expand, 'top-right');
