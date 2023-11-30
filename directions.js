751ae2e6679a0037d0748ac4ad3fafe0c2404210
Stuart Reckase <sdreckase@gmail.com>
2023-11-30 12:12:13 -0800

    skeleton code for adding csv directions integration

--- directions.js
+++ directions.js
@@ -1,3 +1,28 @@
+import * as Papa from "papaparse";
+
+// Async function that will return the promise of a read CSV file put in the public directory
+// Fetch is used to look for and load the file
+async function fetchCSV() {
+    const response = await fetch("./directions.csv");
+    const data = await response.text();
+    return data;
+};
+
+// This will parse read CSV data into a JS object of arrays array
+function readCSV(data) {
+    const records = Papa.parse(data);
+    return records;
+};
+
+
+// Everything is going to have to be put in this `then` block bc it has to run AFTER the CSV data is parsed
+fetchCSV().then((csvData) => {
+    const parsedCSV = readCSV(csvData);
+    console.log(parsedCSV);
+})
+
+
+
 var testDirectionsArray1 = ["point1", "point2", "point3"];
 var testDirectionsArray2 = ["point3", "point4", "point5"];
 
